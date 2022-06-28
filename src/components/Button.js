3c9a5ab792cb29dc9596e8eb12618752a0add3c3
darikmohammed <r.m.darik@gmail.com>
2022-06-29 01:49:49 +0300

    Add Styling

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import './Style/Button.css';
 import PropTypes from 'prop-types';
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class Button extends Component {
   render() {
-    const { value, key } = this.props;
+    const { value } = this.props;
     return (
-      <div className="btn" key={key}>
+      <div className="btn" id={`btn${value}`}>
         {value}
       </div>
     );
@@ -15,5 +16,4 @@ export default class Button extends Component {
 
 Button.propTypes = {
   value: PropTypes.string.isRequired,
-  key: PropTypes.string.isRequired,
 };
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Style/Calculator.css';
 import PropTypes from 'prop-types';
 import Button from './Button';
 
