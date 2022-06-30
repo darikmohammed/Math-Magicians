fbda205eb04f9122529911e570b058f464adbf15
darikmohammed <r.m.darik@gmail.com>
2022-06-30 21:48:35 +0300

    Refactor app  component

--- src/App.js
+++ src/App.js
@@ -1,37 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Calculator from './components/Calculator';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.buttons = [
-      'AC',
-      '+/-',
-      '%',
-      '÷',
-      '7',
-      '8',
-      '9',
-      'x',
-      '4',
-      '5',
-      '6',
-      '-',
-      '1',
-      '2',
-      '3',
-      '+',
-      '0',
-      '.',
-      '=',
-    ];
-  }
-
-  render() {
-    return (
-      <div>
-        <Calculator calculatorButtons={this.buttons} />
-      </div>
-    );
-  }
+function App() {
+  const buttons = [
+    'AC',
+    '+/-',
+    '%',
+    '÷',
+    '7',
+    '8',
+    '9',
+    'x',
+    '4',
+    '5',
+    '6',
+    '-',
+    '1',
+    '2',
+    '3',
+    '+',
+    '0',
+    '.',
+    '=',
+  ];
+  return (
+    <div>
+      <Calculator calculatorButtons={buttons} />
+    </div>
+  );
 }
+
+export default App;
